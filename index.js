16fbc159919f7357aeb9cc5b3a072245b7e0c796
hkeidenb <hasani.eidenberger@gmail.com>
2024-02-24 21:07:06 -0800

    Variables/ prompts added

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = ["What is the title of your webpage?", "Can you describe your webpage?", "What are the installation instructions?", "How do you use the webpage?", "what license did you use? "];
+
+inquirer.prompt (
+    [
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        }
+
+        { 
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        }
+
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'instructions',
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        }
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'usage',
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        }
+        {
+            type: 'list',
+            message: questions[4],
+            name: "installation",
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        }
+    ]
+
+)
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
