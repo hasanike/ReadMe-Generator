2729c0f60712dcbabfaf58b645c8088dc44a21aa
hkeidenb <hasani.eidenberger@gmail.com>
2024-02-24 21:43:10 -0800

    Write file function

--- index.js
+++ index.js
@@ -1,54 +1,84 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
 
+// an array for user Input
+const questions = ["What is the title of your webpage?", "Can you describe your webpage?", "What are the installation instructions?", "How do you use the webpage?", "Who should be credited for working on the webpage?", "Did you run tests?", "Do you have a license?","What is your Github?", "What is your email?"];
 
-const questions = ["What is the title of your webpage?", "Can you describe your webpage?", "What are the installation instructions?", "How do you use the webpage?", "what license did you use? "];
 
-inquirer.prompt (
-    [
+// function to write README file
+function writeToFile(fileName, data) { 
+    const info = generator(data);
+    fs.writeFile(fileName, info , (err) =>
+        err ? console.error(err) : console.log('Success!')
+        );
+
+}
+//  function to initialize app
+function init() {
+inquirer
+    .prompt([
         {
             type: 'input',
             message: questions[0],
             name: 'title',
             validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
-        }
+        },
 
         { 
             type: 'input',
             message: questions[1],
             name: 'description',
             validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
-        }
+        },
 
         {
             type: 'input',
             message: questions[2],
             name: 'instructions',
             validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
-        }
+        },
         {
             type: 'input',
             message: questions[3],
             name: 'usage',
             validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
-        }
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: "contributions",
+            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: "tests",
+
+        },
         {
             type: 'list',
-            message: questions[4],
-            name: "installation",
-            validate: (value) => {if(value){return true} else {return "I need a value to continue"}}
-        }
-    ]
+            message: questions[6],
+            name: 'license',
+            choices: ["MIT", "ISC", "GPLv3", "None"],
+        },
 
-)
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+        {   type: 'list',
+            message: questions[7],
+            name: 'github',
+        },
+        {   
+            type: 'list',
+            message: questions[8],
+            name: 'email',
+            
+        },
+    ])
+    .then((response) => {
+        
+        writeToFile('sampleReadMe.md', response);
+    });
+}
 
 // Function call to initialize app
 init();
